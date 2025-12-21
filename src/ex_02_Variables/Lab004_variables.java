3ba29fddc1a56ff8dbd4f33fc1af3b5f5ea7dd33
Kshitij Shukla <shukla7009@gmail.com>
2025-12-21 18:20:24 +0530

    Variables practice

---
+++ src/ex_02_Variables/Lab004_variables.java
@@ -0,0 +1,19 @@
+package ex_02_Variables;
+
+public class Lab004_variables {
+
+
+    public static void main(String[] args) {
+        String name = "Hashirama";
+        int age = 29;
+        double height = 172.5;
+        boolean islearningjava = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(islearningjava);
+
+
+    }
+}
